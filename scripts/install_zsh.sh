008d922d70d2b6e87536dbd07ed50e3ecd3237ff
Sebastian Felipe Landinez Garcia <sebastian.garcia@codescrum.com>
2016-09-20 09:43:13 -0500

    wip

--- scripts/install_zsh.sh
+++ scripts/install_zsh.sh
@@ -3,16 +3,28 @@
 # The first step is to install zsh.
 brew install zsh zsh-completions
 
-# Install zplug, the manager of plugins we will use.
-curl -sL zplug.sh/installer | zsh
-
-# Add the zsh-syntax-highlighting plugin
-if [ -z $(cat ~/.zshrc | grep "plugins=(git bundler colorized brew zeus gem rails ruby npm node nano nanoc history-substring-search)") ]; then
-  echo "plugins=(git bundler colorized brew zeus gem rails ruby npm node nano nanoc history-substring-search)" >> ~/.zshrc
-fi
-if [ -z $(cat ~/.zshrc | grep "zplug\"zsh-users/zsh-syntax-highlighting\", nice:10") ]; then
-  echo "zplug\"zsh-users/zsh-syntax-highlighting\", nice:10" >> ~/.zshrc
+# Install zplug, the manager of plugins we will use. Just install if it is not installed
+if [[ ! -a  ~/.zplug ]]; then
+  curl -sL zplug.sh/installer | zsh
+else
+  echo "Zplug already installed"
 fi
 
 # Update the zsh console manually to run zsh
 chsh -s $(which zsh)
+
+# Insert line by line configurations to the zsh, if not existing
+insert_line_into_file "plugins=(git bundler colorized brew zeus gem rails ruby npm node nano nanoc history-substring-search)" ~/.zshrc
+insert_line_into_file "# Zplug plugins" ~/.zshrc
+insert_line_into_file "# zplug \"zsh-users/zsh-syntax-highlighting\", nice: 10" ~/.zshrc
+
+# Source the changes in ~/.zshrc
+source ~/.zshrc
+
+
+
+insert_line_into_file(){
+  line=$0
+  file=$1
+  grep -q "$line" "$file" || echo "$line" >> "$file"
+}

11036d47b0a6a914c99d7fc423d34b66de53c17a
Sebastian Felipe Landinez Garcia <sebastian.garcia@codescrum.com>
2016-09-20 10:37:30 -0500

    wip. Changed zplug installation mode to bew

--- scripts/install_zsh.sh
+++ scripts/install_zsh.sh
@@ -4,21 +4,32 @@ main(){
   # The first step is to install zsh.
   brew install zsh zsh-completions
 
-  # Install zplug, the manager of plugins we will use. Just install if it is not installed
-  if [[ ! -a  ~/.zplug ]]; then
-    curl -sL zplug.sh/installer | zsh
-  else
-    echo "Zplug already installed"
-  fi
+  # Install zplug, the manager of plugins we will use.
+  brew install zplug
 
   # Update the zsh console manually to run zsh
   chsh -s $(which zsh)
 
   # Insert line by line configurations to the zsh, if not existing
-  insert_line_into_file "plugins=(git bundler colorized brew zeus gem rails ruby npm node nano nanoc history-substring-search)" ~/.zshrc
+  # insert_line_into_file "plugins=(git bundler colorized brew zeus gem rails ruby npm node nano nanoc history-substring-search)" ~/.zshrc
+  insert_line_into_file "export ZPLUG_HOME=/usr/local/opt/zplug" ~/.zshrc
+  insert_line_into_file "source \$ZPLUG_HOME/init.zsh" ~/.zshrc
   insert_line_into_file "# Zplug plugins" ~/.zshrc
-  insert_line_into_file "source ~/.zplug/init.zsh" ~/.zshrc
+  insert_line_into_file "zplug \"plugins/git\",     from: oh-my-zsh" ~/.zshrc
+  insert_line_into_file "zplug \"plugins/bundler\",     from: oh-my-zsh" ~/.zshrc
+  insert_line_into_file "zplug \"plugins/colorized\",     from: oh-my-zsh" ~/.zshrc
+  insert_line_into_file "zplug \"plugins/brew\",     from: oh-my-zsh" ~/.zshrc
+  insert_line_into_file "zplug \"plugins/zeus\",     from: oh-my-zsh" ~/.zshrc
+  insert_line_into_file "zplug \"plugins/gem\",     from: oh-my-zsh" ~/.zshrc
+  insert_line_into_file "zplug \"plugins/rails\",     from: oh-my-zsh" ~/.zshrc
+  insert_line_into_file "zplug \"plugins/ruby\",     from: oh-my-zsh" ~/.zshrc
+  insert_line_into_file "zplug \"plugins/npm\",     from: oh-my-zsh" ~/.zshrc
+  insert_line_into_file "zplug \"plugins/node\",     from: oh-my-zsh" ~/.zshrc
+  insert_line_into_file "zplug \"plugins/nano\",     from: oh-my-zsh" ~/.zshrc
+  insert_line_into_file "zplug \"plugins/nanoc\",     from: oh-my-zsh" ~/.zshrc
+  insert_line_into_file "zplug \"plugins/history-substring-search\",     from: oh-my-zsh" ~/.zshrc
   insert_line_into_file "zplug \"zsh-users/zsh-syntax-highlighting\", nice: 10" ~/.zshrc
+  insert_line_into_file "zplug install" ~/.zshrc
 
   # Source the changes in ~/.zshrc
   source ~/.zshrc

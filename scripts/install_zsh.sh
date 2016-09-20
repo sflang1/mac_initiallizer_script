6ea020a9834753c98d211d599f391dadd000cbc3
Sebastian Felipe Landinez Garcia <sebastian.garcia@codescrum.com>
2016-09-20 09:59:03 -0500

    Added zplug initializer line

--- scripts/install_zsh.sh
+++ scripts/install_zsh.sh
@@ -17,6 +17,7 @@ main(){
   # Insert line by line configurations to the zsh, if not existing
   insert_line_into_file "plugins=(git bundler colorized brew zeus gem rails ruby npm node nano nanoc history-substring-search)" ~/.zshrc
   insert_line_into_file "# Zplug plugins" ~/.zshrc
+  insert_line_into_file "source ~/.zplug/init.zsh" ~/.zshrc
   insert_line_into_file "zplug \"zsh-users/zsh-syntax-highlighting\", nice: 10" ~/.zshrc
 
   # Source the changes in ~/.zshrc

f8301b208ca05dd99160738bfa319803f9951cf3
Sebastian Felipe Landinez Garcia <sebastian.garcia@codescrum.com>
2016-09-29 12:34:16 -0500

    new order in installing the script

--- dots.sh
+++ dots.sh
@@ -3,14 +3,14 @@
 # Created by Sebastián Landínez, based on https://github.com/gato-omega/my-dots
 set -eu   # Make that the errors in every script stop the execution of the general script.
 
+
+# First of all, install brew and some other required installers (core-utils also for the realpath, etc.)
+sh scripts/binaries.sh
+# Put some defaults related to the environment
 export dirname=$(dirname $(realpath $0))
 export lib="$dirname/lib"
 export SHELL_LIBRARY_PATH="$dirname/lib"
-
-# Put some defaults related to the environment
 sh scripts/defaults.sh
-# First of all, install brew and some other required installers
-sh scripts/binaries.sh
 # I think the first should be install ZSH and the plugin managers, in this case, we'll use
 # zPlug, can be found in: https://github.com/zplug/zplug
 sh scripts/install_zsh.sh

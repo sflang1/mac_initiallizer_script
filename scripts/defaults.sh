fba1782ffab68d0967a505a3954661753d0a86ac
Sebastian Felipe Landinez Garcia <sebastian.garcia@codescrum.com>
2016-09-29 10:21:50 -0500

    Temp issue

--- scripts/defaults.sh
+++ scripts/defaults.sh
@@ -84,6 +84,10 @@ sudo -v
 # Keep-alive: update existing `sudo` time stamp until `.osx` has finished
 while true; do sudo -n true; sleep 60; kill -0 "$$" || exit; done 2>/dev/null &
 
+export dirname=$(dirname $(realpath $0))
+export lib="$dirname/lib"
+export SHELL_LIBRARY_PATH="$dirname/lib"
+
 echo "This script will make your Mac awesome"
 
 ###############################################################################

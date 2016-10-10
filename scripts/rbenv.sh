746dc9c63978e46a270f192ae40f1b8da6285e9e
Sebastian Felipe Landinez Garcia <sebastian.garcia@codescrum.com>
2016-10-10 15:54:13 -0500

    New installation method for the rbenv, including some plugins

--- scripts/rbenv.sh
+++ scripts/rbenv.sh
@@ -2,12 +2,59 @@
 main(){
   # Install Rubyenv for managing the Ruby versions
   # First of all, check if Git is installed
-  if [[ ! $(which git) ]]; then
-    echo "Git is not installed."
-    return
+  # Verify Git is installed:
+  if [ ! $(which git) ]; then
+    echo "Git is not installed, can't continue."
+    exit 1
   fi
 
+  if [ -z "${RBENV_ROOT}" ]; then
+    RBENV_ROOT="$HOME/.rbenv"
+  fi
 
+  # Install rbenv:
+  if [ ! -d "$RBENV_ROOT" ] ; then
+    git clone https://github.com/sstephenson/rbenv.git $RBENV_ROOT
+  else
+    cd $RBENV_ROOT
+    if [ ! -d '.git' ]; then
+      git init
+      git remote add origin https://github.com/sstephenson/rbenv.git
+    fi
+    git pull origin master
+  fi
+
+  # Install plugins:
+  PLUGINS=(
+    sstephenson/rbenv-vars
+    sstephenson/ruby-build
+    sstephenson/rbenv-default-gems
+    sstephenson/rbenv-gem-rehash
+    fesplugas/rbenv-installer
+    fesplugas/rbenv-bootstrap
+    rkh/rbenv-update
+    rkh/rbenv-whatis
+    rkh/rbenv-use
+  )
+
+  for plugin in ${PLUGINS[@]} ; do
+
+    KEY=${plugin%%/*}
+    VALUE=${plugin#*/}
+
+    RBENV_PLUGIN_ROOT="${RBENV_ROOT}/plugins/$VALUE"
+    if [ ! -d "$RBENV_PLUGIN_ROOT" ] ; then
+      git clone https://github.com/$KEY/$VALUE.git $RBENV_PLUGIN_ROOT
+    else
+      cd $RBENV_PLUGIN_ROOT
+      echo "Pulling $VALUE updates."
+      git pull
+    fi
+
+  done
+
+  # Just in case, rbenv rehash
+  rbenv rehash
   ################################ RBENV INSTALLATION ################################
   # Rbenv not installed
   # OLD INSTALLATION PROCESS. DEPRECATED FOR RBENV INSTALLER
@@ -19,8 +66,6 @@ main(){
   #   # Ruby build installation, for using the command rbenv install
   #   git clone https://github.com/rbenv/ruby-build.git ~/.rbenv/plugins/ruby-build
   # fi
-  # Run Rbenv installer.
-  curl -fsSL https://github.com/rbenv/rbenv-installer/raw/master/bin/rbenv-installer | bash
 }
 
 insert_line_into_file(){
